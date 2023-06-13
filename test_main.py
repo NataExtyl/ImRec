4f1b67682e17e814b895b074397f941dff2c16d5
Nataliya Antropova <86189611+NataAntro@users.noreply.github.com>
2023-06-13 03:08:43 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,15 +1,13 @@
-import io
-import pytest
 import numpy as np
 from PIL import Image
 from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
-from tensorflow.keras.preprocessing import image
 from streamlit_app import load_model, preprocess_image, print_predictions
+
 
 def test_load_model():
     model = load_model()
     assert isinstance(model, EfficientNetB0), "Model loading failed, not instance of EfficientNetB0"
+
 
 def test_preprocess_image():
     # создание тестового изображения
@@ -18,6 +16,7 @@ def test_preprocess_image():
     processed_image = preprocess_image(img)
     assert processed_image.shape == (1, 224, 224, 3), "Preprocess image function is not working correctly"
 
+
 def test_print_predictions():
     # для этого теста нам нужно проверить, работает ли функция без ошибок
     # так как мы не можем предсказать конкретный вывод функции
@@ -25,5 +24,5 @@ def test_print_predictions():
     try:
         print_predictions(predictions)
         assert True, "Print predictions function works correctly"
-    except:
+    except Exception:
         assert False, "Print predictions function is not working correctly"
