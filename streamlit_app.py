31206c6026919b072c45620d9c9680af77353934
Nataliya Antropova <86189611+NataAntro@users.noreply.github.com>
2023-06-13 03:06:21 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,26 +4,27 @@ import numpy as np
 import streamlit as st
 from PIL import Image
 from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras.applications.efficientnet import preprocess_input
-from tensorflow.keras.applications.efficientnet import decode_predictions
-from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing import image
 
 
-@st.cache_resource()
+st.cache_resource()
+
+
 def load_model():
     return EfficientNetB0(weights='imagenet')
 
 
 def preprocess_image(img):
     img = img.resize((224, 224))
-    x = img_to_array(img)
+    x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
     return x
 
 
 def load_image():
-    uploaded_file = st.file_uploader('Выберите изображение для распознавания ')
+    uploaded_file = st.file_uploader(label='Выберите изображение для распознавания ')
     if uploaded_file is not None:
         image_data = uploaded_file.getvalue()
         st.image(image_data)
@@ -38,22 +39,17 @@ def print_predictions(preds):
         st.write(cl[1], cl[2])
 
 
-def main():
-    model = load_model()
-    st.title('Распознование изображений в облаке STREAMLIT')
-    st.markdown('Проект по программной инженерии')
-    st.text('Антропова Н.Г.')
+model = load_model()
+st.title('Распознование изображений в облаке STREAMLIT')
+st.markdown('Проект по программной инженерии')
+st.text('Антропова Н.Г.')
 
-    img = load_image()
-    result = st.button('Распознать изображение')
-    if result:
-        with st.spinner('Подождите...'):
-            x = preprocess_image(img)
-            preds = model.predict(x)
-            st.write('Результат:')
-            print_predictions(preds)
-        st.success('Готово!')
-
-
-if __name__ == "__main__":
-    main()
+img = load_image()
+result = st.button('Распознать изображение')
+if result:
+    with st.spinner('Подождите...'):
+        x = preprocess_image(img)
+        preds = model.predict(x)
+        st.write('Результат:')
+        print_predictions(preds)
+    st.success('Готово!')
