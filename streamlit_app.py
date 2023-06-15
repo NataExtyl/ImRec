1b50bcf8617ae58f4ffd104fac68a67b840a9150
Nataliya Antropova <86189611+NataAntro@users.noreply.github.com>
2023-06-15 12:05:13 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ from tensorflow.keras.preprocessing import image
 
 st.cache_resource()
 
-openai.api_key = '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'
+openai.api_key = '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'
 
 
 def send_to_openai(preds):
