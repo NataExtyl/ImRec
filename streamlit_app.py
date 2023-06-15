f7f144c96b69449c16cc761359fb46b4ac3aae7d
NataExtyl <134370085+NataExtyl@users.noreply.github.com>
2023-06-15 10:50:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import io
-
+import openai
 import numpy as np
 import streamlit as st
 from PIL import Image
@@ -11,6 +11,26 @@ from tensorflow.keras.applications.efficientnet import (
 from tensorflow.keras.preprocessing import image
 
 st.cache_resource()
+
+openai.api_key = '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'
+
+
+def send_to_openai(preds):
+    classes = decode_predictions(preds, top=3)[0]
+    predictions = [f"{cl[1]} {cl[2]}" for cl in classes]
+    prompt = (
+        "Результаты распознавания изображения следующие: "
+        + ", ".join(predictions)
+        + " Прокомментируй содержание изображения по полученным данным"
+    )
+
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1000
+    )
+
+    return response.choices[0].text.strip()
 
 
 def load_model():
@@ -43,7 +63,7 @@ def print_predictions(preds):
 
 
 model = load_model()
-st.title('Распознование изображений в облаке STREAMLIT')
+st.title('Распознавание изображений в облаке STREAMLIT')
 st.markdown('Проект по программной инженерии')
 st.text('Антропова Н.Г.')
 
@@ -55,4 +75,7 @@ if result:
         preds = model.predict(x)
         st.write('Результат:')
         print_predictions(preds)
+        st.write('Комментарий к изображению:')
+        st.write(send_to_openai(preds))
+
     st.success('Готово!')
