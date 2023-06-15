ea40c1d6b102ad3f233f2f552450d745ba4bb135
Nataliya Antropova <86189611+NataAntro@users.noreply.github.com>
2023-06-15 10:58:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,18 @@ from tensorflow.keras.applications.efficientnet import (
 )
 from tensorflow.keras.preprocessing import image
 
-st.cache_resource()
-
+# Устанавливаем ключ API для OpenAI
 openai.api_key = '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'
 
 
 def send_to_openai(preds):
+    """
+    Эта функция принимает предсказания модели и формирует запрос к OpenAI.
+    Затем возвращает текст, сгенерированный AI.
+
+    :param preds: массив с результатами предсказаний модели
+    :return: строка с текстом, сгенерированным AI
+    """
     classes = decode_predictions(preds, top=3)[0]
     predictions = [f"{cl[1]} {cl[2]}" for cl in classes]
     prompt = (
@@ -34,10 +40,22 @@ def send_to_openai(preds):
 
 
 def load_model():
+    """
+    Функция загружает предобученную модель EfficientNetB0 с весами 'imagenet'
+
+    :return: модель EfficientNetB0
+    """
     return EfficientNetB0(weights='imagenet')
 
 
 def preprocess_image(img):
+    """
+    Функция предобрабатывает изображение для дальнейшего использования в модели.
+    Изображение приводится к размеру (224, 224), нормализуется и добавляется размерность для батча.
+
+    :param img: изображение в формате PIL.Image
+    :return: обработанное изображение
+    """
     img = img.resize((224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
@@ -46,6 +64,11 @@ def preprocess_image(img):
 
 
 def load_image():
+    """
+    Функция загружает изображение из интерфейса Streamlit.
+
+    :return: изображение в формате PIL.Image или None, если изображение не было загружено
+    """
     label = 'Выберите изображение для распознавания '
     uploaded_file = st.file_uploader(label=label)
     if uploaded_file is not None:
@@ -57,25 +80,41 @@ def load_image():
 
 
 def print_predictions(preds):
+    """
+    Функция выводит в интерфейс Streamlit предсказания модели.
+
+    :param preds: массив с результатами предсказаний модели
+    """
     classes = decode_predictions(preds, top=3)[0]
     for cl in classes:
         st.write(cl[1], cl[2])
 
 
+# Загружаем модель
 model = load_model()
+
+# Настраиваем заголовки для интерфейса Streamlit
 st.title('Распознавание изображений в облаке STREAMLIT')
 st.markdown('Проект по программной инженерии')
 st.text('Антропова Н.Г.')
 
+# Загружаем изображение
 img = load_image()
+
+# Если пользователь нажал кнопку "Распознать изображение"
 result = st.button('Распознать изображение')
 if result:
     with st.spinner('Подождите...'):
+        # Предобрабатываем изображение
         x = preprocess_image(img)
+        # Делаем предсказание
         preds = model.predict(x)
         st.write('Результат:')
+        # Выводим предсказания
         print_predictions(preds)
         st.write('Комментарий к изображению:')
+        # Генерируем и выводим комментарий с помощью AI
         st.write(send_to_openai(preds))
 
+    # Сообщаем пользователю, что обработка завершена
     st.success('Готово!')
